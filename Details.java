eee14138053ba6e934eb36bdd92a85aef7b71317
srinithyaerasem <113082785+srinithyaerasem@users.noreply.github.com>
2025-09-14 21:42:24 +0530

    Add files via upload

---
+++ Details.java
@@ -0,0 +1,9 @@
+public class Details{
+	public static void main(String [] args){
+		System.out.println("SRI NITHYA ERASEM ");
+		System.out.println(" ERASEM NARENDRA BABU");
+		System.out.println(" GUNTUR");
+		System.out.println("Andhra Pradesh ");
+		System.out.println("SRI NITHYA ERASEM \n GUNTUR \n Andhra Pradesh ");
+	}	
+}
