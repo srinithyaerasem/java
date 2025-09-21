b43ae1fbbf31cad95cb82331d8df2482eb4de390
srinithyaerasem <113082785+srinithyaerasem@users.noreply.github.com>
2025-09-21 23:30:45 +0530

    Add files via upload

---
+++ MinandMax.java
@@ -0,0 +1,16 @@
+
+
+class MinandMax {
+    public static void main(String[] args) {
+        
+        System.out.println("Byte min value: "+Byte.MIN_VALUE+" AND Byte max value: " +Byte.MIN_VALUE);
+        System.out.println("Short min value: "+Short.MIN_VALUE+" AND Short max value: "+Short.MAX_VALUE);
+	System.out.println("Integer min value: " + Integer.MIN_VALUE+" AND Integer max value: "+Integer.MAX_VALUE); 
+	System.out.println("Long min value: "+Long.MIN_VALUE+" AND Long max value: "+Long.MAX_VALUE);	
+	System.out.println("float min value: "+Float.MIN_VALUE+" AND Float max value: "+Float.MAX_VALUE);
+	System.out.println("Double min value: "+Double.MIN_VALUE+" AND Double max value: "+Double.MAX_VALUE);
+	System.out.println("Char min value: "+WrapperClass.MIN_VALUE+" AND Char max value: "+WrapperClass.MAX_VALUE);
+	
+
+    }
+}
