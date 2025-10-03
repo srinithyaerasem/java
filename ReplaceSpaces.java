d10c70c1f3a739f288d0261a8e4a3dcc9d7fe09d
srinithyaerasem <113082785+srinithyaerasem@users.noreply.github.com>
2025-10-03 14:11:40 +0530

    Add files via upload

---
+++ ReplaceSpaces.java
@@ -0,0 +1,10 @@
+package java10k;
+
+public class ReplaceSpaces {
+	public static void main(String[] args) {
+		String s1="Hi hello how are you";
+		String res= s1.replace(" ", "%20");
+		System.out.println(res);
+	}
+
+}
