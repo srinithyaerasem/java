2d94a1fed947a9210d65ced01c7ff2fbf7e568f8
srinithyaerasem <113082785+srinithyaerasem@users.noreply.github.com>
2025-10-05 22:02:54 +0530

    Add files via upload

---
+++ PrimeJump.java
@@ -0,0 +1,24 @@
+package java10k;
+
+public class PrimeJump {
+	public static void main(String[] args) {
+		for(int i=1;i<=100;i++) {
+			int count=0;
+			for(int j=1;j<=i;j++) {
+				if(i%j==0) {
+					count++;
+				}
+				
+			}
+			if(count==2) {
+				if(i>=10) {
+					break;
+				}
+				System.out.println(i);
+				
+			}
+			
+		}
+	}
+
+}
