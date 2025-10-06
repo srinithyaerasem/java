c8c4adec74fd41277cb7467008645f3baed81cb6
srinithyaerasem <113082785+srinithyaerasem@users.noreply.github.com>
2025-10-06 16:05:58 +0530

    Add files via upload

---
+++ RemoveSpaces.java
@@ -0,0 +1,10 @@
+package java10k;
+
+public class RemoveSpaces {
+	public static void main(String[] args) {
+		String s1 ="Hi hello How are you";
+		String res= s1.replace(" ", "");
+		System.out.println(res);
+	}
+
+}
