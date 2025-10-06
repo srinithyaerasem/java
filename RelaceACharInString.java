286d95e4b3c4c12765642b425b3ae938e3f27ba7
srinithyaerasem <113082785+srinithyaerasem@users.noreply.github.com>
2025-10-06 16:05:41 +0530

    Add files via upload

---
+++ RelaceACharInString.java
@@ -0,0 +1,10 @@
+package java10k;
+
+public class RelaceACharInString {
+	public static void main(String[] args) {
+		String s2="hello";
+        String result= s2.replace('l', 'z');
+        System.out.println(result);
+	}
+
+}
