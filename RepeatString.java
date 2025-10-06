43c6713d817b722d133164f648de8828aaca9dde
srinithyaerasem <113082785+srinithyaerasem@users.noreply.github.com>
2025-10-06 16:06:11 +0530

    Add files via upload

---
+++ RepeatString.java
@@ -0,0 +1,16 @@
+package java10k;
+
+public class RepeatString {
+	public static void main(String[] args) {
+        String s1= "abc";
+        int num =3;
+        String res="";
+        for(int i=0;i<num;i++){
+            res=res+s1;
+        }
+        System.out.println(res);
+        //replace
+        
+    }
+
+}
