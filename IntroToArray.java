81f29bf8cae73037c1b375fa053ebe99acdf0078
srinithyaerasem <113082785+srinithyaerasem@users.noreply.github.com>
2025-10-13 23:01:14 +0530

    Add files via upload

---
+++ IntroToArray.java
@@ -0,0 +1,14 @@
+package java10k;
+
+public class IntroToArray {
+	public static void main(String[] args) {
+		//Implicitly declaring
+		int[] arr= {1,2,3,4,5};
+		//this prints the location
+		System.out.println(arr);
+		for(int i=0;i<arr.length;i++) {
+			System.out.println(arr[i]);
+		}
+	}
+
+}
