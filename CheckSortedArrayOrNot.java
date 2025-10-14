9223d101541a4551c7d4f60caebdfb7335d73eb5
srinithyaerasem <113082785+srinithyaerasem@users.noreply.github.com>
2025-10-14 22:51:34 +0530

    Add files via upload

---
+++ CheckSortedArrayOrNot.java
@@ -0,0 +1,19 @@
+package Java10000.arrayslearn;
+
+public class CheckSortedArrayOrNot {
+	public static boolean CheckSorthed(int[] nums) {
+		for(int i=1;i<nums.length;i++) {
+			if(nums[i]<nums[i-1]) {
+				return false;
+			}
+		}
+			return true;
+				
+	}
+	public static void main(String[] args) {
+		int[] nums= {1,2,3,4,5,6,7};
+		System.out.println(CheckSorthed(nums));
+		
+	}
+
+}
