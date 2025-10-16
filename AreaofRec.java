165c44aa8d00098da70ae429adf55598615a2601
srinithyaerasem <113082785+srinithyaerasem@users.noreply.github.com>
2025-10-16 22:42:05 +0530

    Add files via upload

---
+++ AreaofRec.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class AreaofRec {
+    public static void main(String[] args) {
+        
+        Scanner sc= new Scanner(System.in);
+        System.out.print("Enter length of rectangle:");
+        int len=sc.nextInt();
+        System.out.print("Enter breadth of rectangle:");
+        int br = sc.nextInt();
+        double ans = 0.5*len*br;
+        System.out.print("Enter area of rectangle is:"+ans);
+        
+
+
+    }
+    
+}
