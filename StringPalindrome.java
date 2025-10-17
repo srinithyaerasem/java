cabcdd5dd20f1a6f01be6dd6cf4f75d85523276f
srinithyaerasem <113082785+srinithyaerasem@users.noreply.github.com>
2025-10-17 22:39:52 +0530

    Add files via upload

--- StringPalindrome.java
+++ StringPalindrome.java
@@ -1,21 +1,19 @@
-package java10k;
-
+import java.util.*;
 public class StringPalindrome {
-
-	public static void main(String[] args) {
-		String s="abab";
-		String ans="";
-		for(int i=s.length()-1;i>=0;i--) {
-			ans=ans+s.charAt(i);
-		}
-		if(s.equals(ans)) {
-			System.out.println("Palindrome");
-		}
-		else {
-			System.out.println("Not palindrome");
-		}
-		System.out.println("The given string is:"+s);
-		System.out.println("The reversed string is: "+ans);
-	}
-
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        String rev="";
+        int len = str.length();
+        for(int i=len-1;i>=0;i--){
+            rev=rev+str.charAt(i);
+        } 
+        if(str.equals(rev)){
+            System.out.println("Palindrome");
+        }
+        else{
+            System.out.println("Not Palindrome");
+        }
+    }
+    
 }
