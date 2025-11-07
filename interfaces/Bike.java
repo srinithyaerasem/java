ea8c51f69fa725d395f9456f00b96989a6da1d68
srinithyaerasem <113082785+srinithyaerasem@users.noreply.github.com>
2025-11-07 21:37:32 +0530

    Implement Bike class with Vehicle interface methods

---
+++ interfaces/Bike.java
@@ -0,0 +1,20 @@
+package com.interfaces;
+
+public class Bike implements Vehicle{
+
+	@Override
+	public void start() {
+		// TODO Auto-generated method stub
+		System.out.println("Bike start with key");
+		
+	}
+
+	@Override
+	public void stop() {
+		// TODO Auto-generated method stub
+		System.out.println("Bike stop with break");
+		
+	}
+	
+
+}
